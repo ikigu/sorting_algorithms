9ff7e82221a8231564e63fbf7418bae5f9251250
Ikigu <ikigu.geo@gmail.com>
2024-02-20 17:05:04 +0300

    solution: selection sort function that sorts A->Z

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: the array to sort
+ * @size: size of @array
+ *
+ * Return: Nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, smallest, temp;
+
+	if (size < 2 || !array)
+		return;
+	
+	for (i = 0; i < size; i++)
+	{
+		smallest = i;
+
+		for (j = i; j < size; j++)
+		{
+			if(array[j] < array[smallest])
+				smallest = j;
+		}
+		
+		if (smallest != i)
+		{
+			temp = array[i];
+			array[i] = array[smallest];
+			array[smallest] = temp;
+			print_array(array, size);
+		}
+	}
+}
+
