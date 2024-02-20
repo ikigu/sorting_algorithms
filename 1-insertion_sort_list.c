0ecbe98b599855e32c03f0c12b07afff4bc08c3a
Ikigu <ikigu.geo@gmail.com>
2024-02-20 14:54:39 +0300

    solution: function that sorts DLL A->Z using insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly-linked list of ints in ascending order
+ * @list: list to sort
+ *
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *prev, *next;
+
+	current = *list;
+
+	while (current->next)
+	{
+		next = current->next;
+		prev = current->prev;
+
+		if (current->n > next->n)
+		{
+			current->next = next->next;
+			
+			if (next->next)
+				next->next->prev = current;
+
+			next->prev = prev;
+			current->prev = next;
+			next->next = current;
+
+
+			if (prev)
+				prev->next = next;
+			else
+				*list = next;
+
+			current = *list;
+			print_list(*list);
+			continue;
+		}
+
+		current = current->next;
+	}
+}
