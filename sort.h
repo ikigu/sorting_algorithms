44ab21be1747a3f3d5a8984816678ba3a99ce8e4
Ikigu <ikigu.geo@gmail.com>
2024-02-28 08:07:28 +0300

    header: _

--- sort.h
+++ sort.h
@@ -31,5 +31,7 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quicksort_recursion(int *array, int lo, int hi, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
 
 #endif /*SORT_H*/
